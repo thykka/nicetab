6967bbac95546ae98b458a4c996173daa7ea6070
Moses Holmström <thykka@gmail.com>
2018-02-15 20:38:07 +0200

    Added note about API keys

--- build.example.sh
+++ build.example.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Get your API keys here:
+#   https://addons.mozilla.org/en-US/developers/addon/api/key/
+
+# Before first run, install web-ext with:
+#   $ npm install -g web-ext
+
 cd ..
 web-ext sign -s nicetab -v --api-key API_KEY_HERE --api-secret API_SECRET_HERE
 open web-ext-artifacts/
